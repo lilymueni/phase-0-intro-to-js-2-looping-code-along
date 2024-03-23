8076b564b6408c3ee13c61352cd7b0a40b3acdb9
lilianmueninzioki <liliannzioki@gmail.com>
2024-03-23 09:53:36 +0300

    Looping

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+const writeCards = (names, occasion) => {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`);
+    }
+    return messages;
+  };
+  
+  const names = ["whit", "jane", "whishi"];
+  const occasion = "birthday";
+  const messages = writeCards(names, occasion);
+  console.log(messages);
+
+let count = 10;
+function countDown(count){
+    while(count >=0){
+        console.log(count)
+        count --;
+    }
+    return count;
+}
+countDown(count);
